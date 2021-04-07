71b5214a1b303d0ac38f8c3e2a913bd52551affa
Yasen Kostov <qskatak04@gmail.com>
2021-04-07 21:39:05 +0300

    Add alias + mount for gladiators/players

--- lib/tennis_web/live/gladiator_live.ex
+++ lib/tennis_web/live/gladiator_live.ex
@@ -1,8 +1,24 @@
 defmodule TennisWeb.GladiatorLive do
   use TennisWeb, :live_view
 
+  alias Tennis.Players
+
   def render(assigns) do
    render GladiatorsView, "show.html", assigns
+  end
+
+  @impl true
+  def mount(params, %{"admin_token" => admin_token} = session, socket) do
+    gladiator = Tours.get_gladiators!(params["id"])
+    players = Players.list_players
+    admin = Accounts.get_admin_by_session_token(admin_token)
+    socket = assign(
+        socket,
+        gladiator: gladiator,
+        players: players,
+        current_admin: admin
+      )
+    {:ok, socket}
   end
 
 end
