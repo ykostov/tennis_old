accb8f6cefc3ec7181a3266eb5e7bfccbecf76ec
Yasen Kostov <qskatak04@gmail.com>
2021-04-16 14:30:46 +0300

    Unmute module

--- lib/tennis/tours/playersintours.ex
+++ lib/tennis/tours/playersintours.ex
@@ -1,20 +1,20 @@
-# defmodule Tennis.Gladiators.PlayersInTours do
-#   use Ecto.Schema
-#   import Ecto.Changeset
-#
-#   schema "players_in_tours" do
-#
-#     field :gladiator_id, :decimal
-#     field :player_id, :decimal
-#
-#     timestamps()
-#   end
-#
-#   @doc false
-#
-#   def changeset(players_in_tours, attrs) do
-#     player
-#     |> cast(attrs, [:name, :nickname, :email])
-#     |> validate_required([:name, :nickname, :email])
-#   end
-# end
+defmodule Tennis.Gladiators.PlayersInTours do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "players_in_tours" do
+
+    field :gladiator_id, :decimal
+    field :player_id, :decimal
+
+    timestamps()
+  end
+
+  @doc false
+
+  def changeset(players_in_tours, attrs) do
+    player
+    |> cast(attrs, [:name, :nickname, :email])
+    |> validate_required([:name, :nickname, :email])
+  end
+end
