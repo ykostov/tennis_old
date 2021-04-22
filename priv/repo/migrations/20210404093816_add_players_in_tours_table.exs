09cb1827e0ab89d1837c4ece23e84542827aa30e
Yasen Kostov <qskatak04@gmail.com>
2021-04-22 20:11:48 +0300

    Improve migration

--- priv/repo/migrations/20210404093816_add_players_in_tours_table.exs
+++ priv/repo/migrations/20210404093816_add_players_in_tours_table.exs
@@ -2,16 +2,17 @@ defmodule Tennis.Repo.Migrations.AddPlayersInToursTable do
   use Ecto.Migration
 
   def change do
-    create table(:players_in_tours) do
-      add :gladiators, references(:players)
-      add :players, references(:gladiators)
-      add :points, :decimal
-      timestamps()
 
+      create table(:players_in_tours, primary_key: false) do
+        add(:gladiator_id, references(:gladiators, on_delete: :delete_all), primary_key: true)
+        add(:player_id, references(:players, on_delete: :delete_all), primary_key: true)
+        add(:point_id, references(:points, on_delete: :delete_all), primary_key: true)
+
+        timestamps()
+      end
+
+      create(index(:gladiator_tag, [:gladiator_id]))
+      create(index(:gladiator_tag, [:player_id]))
+      create(index(:gladiator_tag, [:point_id]))
     end
-
-    create unique_index(:players_in_tours, [:players, :gladiators])
-
-
   end
-end
