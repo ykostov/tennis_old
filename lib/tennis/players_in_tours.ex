e75233b023b3cc2983ac0c3590f6dcd6fdbc2b51
Yasen Kostov <qskatak04@gmail.com>
2021-04-22 20:34:20 +0300

    Create context

---
+++ lib/tennis/players_in_tours.ex
@@ -0,0 +1,14 @@
+defmodule Tennis.PlayersInTours do
+  @moduledoc """
+  The WebsiteTag context.
+  """
+  import Ecto.Query, warn: false
+  alias Tennis.Repo
+
+  alias Tennis.Tours.Gladiators
+  alias Tennis.Players.Player
+  alias Tennis.Gladiators.PlayersInTours
+  @doc """
+  Returns the list of websites tags.
+  """
+end
