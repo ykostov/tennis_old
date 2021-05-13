5e9bf674171f8466ce2b1ddd0dfa7d87a57d75f8
Yasen Kostov <qskatak04@gmail.com>
2021-05-13 21:17:50 +0300

    Change table name

--- priv/repo/migrations/20210404093816_add_players_in_tours_table.exs
+++ priv/repo/migrations/20210404093816_add_players_in_tours_table.exs
@@ -2,7 +2,7 @@ defmodule Tennis.Repo.Migrations.AddPlayersInToursTable do
   use Ecto.Migration
 
   def change do
-    create table(:players_in_tours) do
+    create table(:gladiator_player) do
       add :gladiators, references(:players)
       add :players, references(:gladiators)
       add :points, :decimal
