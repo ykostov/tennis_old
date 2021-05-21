13c3d5329c825837f1ec02c698a6219aac1cb033
Yasen Kostov <qskatak04@gmail.com>
2021-05-21 11:14:52 +0300

    add player_tour table

---
+++ priv/repo/migrations/20210521075919_add_player_tour_table.exs
@@ -0,0 +1,16 @@
+defmodule Tennis.Repo.Migrations.AddPlayerTourTable do
+  use Ecto.Migration
+
+  def change do
+
+    create table(:player_tour) do
+      add(:player_id, references(:players, on_delete: :delete_all))
+      add(:tour_id, references(:tours, on_delete: :delete_all))
+      add(:points, :decimal)
+    end
+
+    create unique_index(:player_tour, [:player_id, :tour_id], name: :player_tour_unique_index)
+
+
+  end
+end
